df126bd94da4a854d67159af90306d587a4236dd
ili <chudinia@gmail.com>
2024-04-18 14:05:17 +0500

    Дополнены данные поставщика

--- AtolOnline/Service.cs
+++ AtolOnline/Service.cs
@@ -6,6 +6,15 @@
 public class Service
 {
     /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="callbackUrl"><inheritdoc cref="CallbackUrl" path="/summary" /></param>
+    public Service(string? callbackUrl = null)
+    {
+        CallbackUrl = callbackUrl;
+    }
+
+    /// <summary>
     /// URL, на который необходимо ответить после обработки документа.
     /// </summary>
     public string? CallbackUrl { get; set; }
--- AtolOnline/SupplierInfo.cs
+++ AtolOnline/SupplierInfo.cs
@@ -6,6 +6,59 @@
 public class SupplierInfo
 {
     /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="inn"><inheritdoc cref="Inn" path="/summary" /></param>
+    /// <param name="name"><inheritdoc cref="Name" path="/summary" /></param>
+    /// <param name="phones"><inheritdoc cref="Phones" path="/summary" /></param>
+    public SupplierInfo(string inn, string? name = null, List<string>? phones = null)
+    {
+        Inn = inn;
+        Name = name;
+        Phones = phones;
+    }
+
+    /// <summary>
+    /// <para>
+    /// Тег: 1226
+    /// </para>
+    /// ИНН поставщика. 10 или 12 цифр
+    /// </summary>
+    public string Inn { get; }
+
+    /// <summary>
+    /// <para>
+    /// Тег: 1225
+    /// </para>
+    /// Наименование поставщика.
+    /// <para>
+    /// Максимум 256 символов
+    /// </para>
+    /// <para>
+    /// Обязателен если признак агента по предмету расчета принимает следующие значения
+    /// <list type="bullet">
+    /// <item>
+    /// <see cref="AgentType.BankPayingAgent"/>
+    /// </item>
+    /// <item>
+    /// <see cref="AgentType.BankPayingSubagent"/>
+    /// </item>
+    /// <item>
+    /// <see cref="AgentType.PayingAgent"/>
+    /// </item>
+    /// <item>
+    /// <see cref="AgentType.PayingSubagent"/>
+    /// </item>
+    /// </list>
+    /// </para>
+    /// </summary>
+
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// <para>
+    /// Тег: 1171
+    /// </para>
     /// <para>
     /// Номера телефонов платежного агента, платежного субагента, банковского 
     /// платежного агента, банковского платежного субагента
@@ -22,9 +75,23 @@ public class SupplierInfo
     /// «9251234567»). Максимальная длина одного элемента массива в таком случае –
     /// 17 символов
     /// </para>
+    /// <para>
+    /// Обязателен если признак агента по предмету расчета принимает следующие значения
+    /// <list type="bullet">
+    /// <item>
+    /// <see cref="AgentType.BankPayingAgent"/>
+    /// </item>
+    /// <item>
+    /// <see cref="AgentType.BankPayingSubagent"/>
+    /// </item>
+    /// <item>
+    /// <see cref="AgentType.PayingAgent"/>
+    /// </item>
+    /// <item>
+    /// <see cref="AgentType.PayingSubagent"/>
+    /// </item>
+    /// </list>
+    /// </para>
     /// </summary>
-    /// <remarks>
-    /// Тег: 1171
-    /// </remarks>
     public List<string>? Phones { get; set; }
 }
